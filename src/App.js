79b0fc455ff054aa8daf26855a83745c83366c6b
SeungwooYang <dg6800000@gmail.com>
2022-12-07 16:27:15 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>12월 8일 리액트 숙련주차</p>
         <a
           className="App-link"
           href="https://reactjs.org"
